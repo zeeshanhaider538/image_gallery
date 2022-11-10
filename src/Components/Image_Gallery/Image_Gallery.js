23dc617874ebd7bf41405ce5fceb6bed493e2f8a
zeeshanhaider538 <zeeshanhaider538@gmail.com>
2022-11-10 10:28:48 +0500

    repo initialized

---
+++ src/Components/Image_Gallery/Image_Gallery.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Image_Gallery() {
+  return (
+    <div>
+        
+        <div class="loading">
+        <div class="loading-container">
+            <p class="counter"></p>
+            <div class="line"></div>
+        </div>
+        </div>
+        <div class="body-container">
+        <div class="box">
+            <h1>Gallery Example</h1>
+            <p>Pictures from Unsplash</p>
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Image_Gallery
