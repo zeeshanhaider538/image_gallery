0e39f8b0d6a31ea8a27e997aef1f0e7537306fea
zeeshanhaider538 <zeeshanhaider538@gmail.com>
2022-11-10 10:44:37 +0500

    calling and setting the component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Image_Gallery from "./Components/Image_Gallery/Image_Gallery";
 function App() {
   return (
     <div className="App">
-      
+      <Image_Gallery/>
     </div>
   );
 }
